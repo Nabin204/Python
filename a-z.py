6d3a805cf41af6aefb2956a8d8f69822636b314f
Nabin <nabinbhat204@gmail.com>
2024-06-17 21:44:44 +0545

    First Commit

---
+++ a-z.py
@@ -0,0 +1,6 @@
+print("The alphabets in small letter is:")
+for i in range(97,97+26,1):
+    print(chr(i),end="  ")
+print("\nThe alphabets in capital letter is:")
+for i in range(65,65+26):
+    print(chr(i),end="  ")    
