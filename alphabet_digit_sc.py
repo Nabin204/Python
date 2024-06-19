319573f033d4d1b3c29ac5f051776285ae9f01f0
Nabin <nabinbhat204@gmail.com>
2024-06-19 07:49:15 +0545

    Initial Commit

---
+++ alphabet_digit_sc.py
@@ -0,0 +1,7 @@
+a=input("Enter a character:")
+if(a.isalpha()):
+    print("The character is alphabet.")
+elif(a.isdigit()):
+    print("The character is digit.")
+else:
+    print("The character is special character.")
